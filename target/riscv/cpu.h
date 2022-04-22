5f5800d7559bf0832c9bb33316549eb4e675fa9b
Atish Patra <atishp@rivosinc.com>
2022-04-22 15:55:04 -0700

    hw/intc: Move mtimer/mtimecmp to aclint
    Historically, The mtime/mtimecmp has been part of the CPU because
    they are per hart entities. However, they actually belong to aclint
    which is a MMIO device.
    
    Move them to the ACLINT device. This also emulates the real hardware
    more closely.
    
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -308,7 +308,6 @@ struct CPUArchState {
     /* temporary htif regs */
     uint64_t mfromhost;
     uint64_t mtohost;
-    uint64_t timecmp;
 
     /* physical memory protection */
     pmp_table_t pmp_state;
@@ -363,7 +362,6 @@ struct CPUArchState {
     float_status fp_status;
 
     /* Fields from here on are preserved across CPU reset. */
-    QEMUTimer *timer; /* Internal timer */
 
     hwaddr kernel_addr;
     hwaddr fdt_addr;
