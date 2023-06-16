6216d04dcd37598292e46edd2626694a26aeb280
Kaiwen Xue <kaiwenx@rivosinc.com>
2023-06-16 13:57:08 -0700

    target/riscv: Add cycle & instret privilege mode filtering properties
    This adds the properties for ISA extension smcntrpmf. Patches
    implementing it will follow.
    
    Signed-off-by: Kaiwen Xue <kaiwenx@rivosinc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Signed-off-by: Kaiwen Xue <kaiwenx@rivosinc.com>

--- target/riscv/cpu_cfg.h
+++ target/riscv/cpu_cfg.h
@@ -70,6 +70,7 @@ struct RISCVCPUConfig {
     bool ext_zihintpause;
     bool ext_smstateen;
     bool ext_sstc;
+    bool ext_smcntrpmf;
     bool ext_svadu;
     bool ext_svinval;
     bool ext_svnapot;
