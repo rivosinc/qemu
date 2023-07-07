86ac7c65bf316de73a8512e01e7e6918e069869d
Rajnesh Kanwal <rkanwal@rivosinc.com>
2023-07-07 11:20:29 +0100

    target/riscv: Add support to record CTR entries.
    This commit adds logic to records CTR entries of different types
    and adds required hooks in TCG and interrupt/Exception logic to
    record events.
    
    This commit also adds support to invoke freeze CTR logic for breakpoint
    exceptions and counter overflow interrupts.
    
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -129,11 +129,14 @@ DEF_HELPER_2(csrr_i128, tl, env, int)
 DEF_HELPER_4(csrw_i128, void, env, int, tl, tl)
 DEF_HELPER_6(csrrw_i128, tl, env, int, tl, tl, tl, tl)
 #ifndef CONFIG_USER_ONLY
-DEF_HELPER_1(sret, tl, env)
-DEF_HELPER_1(mret, tl, env)
+DEF_HELPER_2(sret, tl, env, tl)
+DEF_HELPER_2(mret, tl, env, tl)
 DEF_HELPER_1(wfi, void, env)
 DEF_HELPER_1(tlb_flush, void, env)
 DEF_HELPER_1(tlb_flush_all, void, env)
+DEF_HELPER_4(ctr_branch, void, env, tl, tl, tl)
+DEF_HELPER_4(ctr_jal, void, env, tl, tl, tl)
+DEF_HELPER_5(ctr_jalr, void, env, tl, tl, tl, tl)
 /* Native Debug */
 DEF_HELPER_1(itrigger_match, void, env)
 #endif
